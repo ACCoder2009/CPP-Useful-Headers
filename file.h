c2b96f16be190f3cff2d06dd23aae4652d0db127
ACCoder2009 <120697184+ACCoder2009@users.noreply.github.com>
2023-07-24 20:33:57 +0800

    read & write files
    A header file that can be used to read and write(including both overriding and appending) from or into text files such as .txt file.

--- file.h
+++ file.h
@@ -3,15 +3,17 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <future>
 
 bool CheckFile(const std::ifstream& stream)
 {
-	if (stream)
-	{
-		std::cout << "FILE IS VALID!" << "\n";
-		return true;
-	}
-	std::cout << "FILE NOT VALID!" << "\n";
+	if (stream) { return true; }
+	return false;
+}
+
+bool CheckFile(const std::ofstream& stream)
+{
+	if (stream) { return true; }
 	return false;
 }
 
@@ -23,11 +25,15 @@ std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vector)
 }
 
 template<typename T>
-std::vector<T> ReadFile(const std::ifstream& stream)
+std::vector<T> ReadFile(std::ifstream& stream)
 {
 	T buffer{};
 	std::vector<T> result;
-	if (!CheckFile(stream)) { return result; }
+	if (!CheckFile(stream))
+	{
+		result.push_back("FILE NOT VALID!");
+		return result;
+	}
 	if (stream.is_open())
 	{
 		while (stream.good())
@@ -45,7 +51,11 @@ std::vector<T> ReadFile(const std::string& filepath)
 	T buffer{};
 	std::vector<T> result;
 	std::ifstream stream(filepath);
-	if (!CheckFile(stream)) { return result; }
+	if (!CheckFile(stream))
+	{
+		result.push_back("FILE NOT VALID!");
+		return result;
+	}
 	if (stream.is_open())
 	{
 		while (stream.good())
@@ -55,4 +65,24 @@ std::vector<T> ReadFile(const std::string& filepath)
 		}
 	}
 	return result;
+}
+
+template<typename T>
+int OverwriteFile(const std::string& filepath, const T& content, bool newline = true)
+{
+	std::ofstream stream(filepath, stream.binary);
+	if (!CheckFile(stream)) { return 1; }
+	if (newline) { stream << content << "\n"; }
+	else { stream << content; }
+	return 0;
+}
+
+template<typename T>
+int AppendFile(const std::string& filepath, const T& content, bool newline = true)
+{
+	std::ofstream stream(filepath, stream.app | stream.binary);
+	if (!CheckFile(stream)) { return 1; }
+	if (newline) { stream << content << "\n"; }
+	else { stream << content; }
+	return 0;
 }
